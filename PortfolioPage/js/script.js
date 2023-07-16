08da5ea189bb8452b7ed9e294477e0c1b6276345
Dras <52209860+DrasChannel@users.noreply.github.com>
2023-07-16 16:52:10 +0200

    Add files via upload

---
+++ PortfolioPage/js/script.js
@@ -0,0 +1,42 @@
+
+
+
+// custom scrollbar init
+var { 
+    OverlayScrollbars, 
+    ScrollbarsHidingPlugin, 
+    SizeObserverPlugin, 
+    ClickScrollPlugin  
+} = OverlayScrollbarsGlobal;
+const osInstance = OverlayScrollbars(document.querySelector('#body'), {});
+
+
+
+//function for checking cookies
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+
+
+// scroll to top function
+function scrolltototop(){
+    window.scrollTo(0, 0);
+}
