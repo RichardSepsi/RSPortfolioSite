4af8178ad0acdba65b967e69de15ec00039f9ce2
Richard Sepši <appsepsi@gmail.com>
2024-04-19 18:27:33 +0200

    fixes and reorganization

---
+++ js/animations.js
@@ -0,0 +1,72 @@
+// Body
+
+anime({
+    targets: '#content-container',
+    opacity: 1,
+    duration: 300,
+    easing: 'cubicBezier(0.500, 0.000, 0.650, 1.000)'
+});
+anime({
+    targets: '#language-selector',
+    opacity: 1,
+    duration: 300,
+    delay: 50,
+    easing: 'cubicBezier(0.500, 0.000, 0.650, 1.000)'
+});
+anime({
+    targets: '.nav-links',
+    opacity: 1,
+    duration: 300,
+    delay: 10,
+    easing: 'cubicBezier(0.500, 0.000, 0.650, 1.000)'
+});
+
+// page fadeout function
+function fadeout(nhref) {
+    anime({
+        targets: '#content-container',
+        opacity: 0,
+        duration: 300,
+        easing: 'cubicBezier(0.500, 0.000, 0.650, 1.000)'
+    });
+    anime({
+        targets: '#language-selector',
+        opacity: 0,
+        duration: 300,
+        easing: 'cubicBezier(0.500, 0.000, 0.650, 1.000)'
+    });
+    anime({
+        targets: '.nav-links',
+        opacity: 0,
+        duration: 300,
+        easing: 'cubicBezier(0.500, 0.000, 0.650, 1.000)'
+    });
+    anime({
+        targets: '.os-theme-dark',
+        opacity: 0,
+        duration: 300,
+        easing: 'cubicBezier(0.500, 0.000, 0.650, 1.000)'
+    });
+
+    // if we are going to homepage, then set the visibility of the small d smoothly back to 0
+    if (nhref == 'home.html') {
+        anime({
+            targets: '.small-d',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'cubicBezier(0.500, 0.000, 1.000, 1.000)',
+            duration: 2000,
+            direction: 'normal',
+        });
+        anime({
+            targets: '.small-d',
+            opacity: 0,
+            easing: 'cubicBezier(0.500, 0.000, 1.000, 1.000)',
+            duration: 300,
+            direction: 'normal',
+        });
+        document.querySelector(".small-d-svg").style.pointerEvents = "none";
+        var smalldvisibility = 0;
+        sessionStorage.setItem("smalldkey", smalldvisibility);
+    }
+    setTimeout(()=> {window.location.href = nhref;} ,300);
+}
