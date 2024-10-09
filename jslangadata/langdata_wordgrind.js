386b16194e5c08f82b3eb03701956200a6fa0438
Richard Sepši <appsepsi@gmail.com>
2024-10-09 19:23:21 +0200

    added wordgrind

---
+++ jslangadata/langdata_wordgrind.js
@@ -0,0 +1,73 @@
+if(currentlang == "EN") {
+    document.getElementById("langs-wordgrind-time").innerHTML = `October 2024`
+    document.getElementById("langs-wordgrind-category").innerHTML = `Category:<span class="project-element-subtext" style="margin-left: 10px;">Web Design</span>`
+    document.getElementById("langs-wordgrind-date").innerHTML = `Date added:<span class="project-element-subtext" style="margin-left: 10px;">9.10.2024</span>`
+    document.getElementById("langs-wordgrind-abouttitle").innerHTML = `About the project`
+    document.getElementById("langs-wordgrind-abouttext").innerHTML = `
+    WordGrind (言葉作業ゲー) is a simple web app for word memorization. It works by randomly picking a word from a deck and asking for an answer.
+    Besides words and their answers there are also "back sides" (made for english meaning of kanji words) and hints, which can all be fully customized.
+    It's a project that I made for my personal use, however if you want, you can also download it from GitHub and use it freely.`
+    document.getElementById("langs-wordgrind-softewaretitle").innerHTML = `Software used`
+    document.getElementById("langs-wordgrind-link").innerHTML = `Get WordGrind on <u>GitHub</u>`
+} else if(currentlang == "SK") {
+    document.getElementById("langs-wordgrind-time").innerHTML = `Oktober 2024`
+    document.getElementById("langs-wordgrind-category").innerHTML = `Kategória:<span class="project-element-subtext" style="margin-left: 10px;">Web Design</span>`
+    document.getElementById("langs-wordgrind-date").innerHTML = `Dátum pridania:<span class="project-element-subtext" style="margin-left: 10px;">9.10.2024</span>`
+    document.getElementById("langs-wordgrind-abouttitle").innerHTML = `O projekte`
+    document.getElementById("langs-wordgrind-abouttext").innerHTML = `
+    WordGrind (言葉作業ゲー) je jednoduchá webová aplikácia pre memorizáciu slov. Funguje tak, že vyberie náhodné slovo zo sady a bude si pýtať odpoveď.
+    Okrem slov a ich odpovedí aplikácia ponúka aj "zadné strany" (urobené pre anglický význam slov s kanji) a nápovedy. Všetko z toho sa dá plne prispôsobiť.
+    Tento projekt som vytvoril pre svoje vlastné používanie, ale ak by ste chceli, tak asi ho môžete aj vy stiahnuť z Githubu a voľne ho používať.`
+    document.getElementById("langs-wordgrind-softewaretitle").innerHTML = `Použitý software`
+    document.getElementById("langs-wordgrind-link").innerHTML = `Získajte WordGrind na <u>GitHube</u>`
+} else if(currentlang == "CZ") {
+    document.getElementById("langs-wordgrind-time").innerHTML = `Říjen 2024`
+    document.getElementById("langs-wordgrind-category").innerHTML = `Kategorie:<span class="project-element-subtext" style="margin-left: 10px;">Web Design</span>`
+    document.getElementById("langs-wordgrind-date").innerHTML = `Datum přidání:<span class="project-element-subtext" style="margin-left: 10px;">9.10.2024</span>`
+    document.getElementById("langs-wordgrind-abouttitle").innerHTML = `O projektu`
+    document.getElementById("langs-wordgrind-abouttext").innerHTML = `
+    WordGrind (言葉作業ゲー) je jednoduchá webová aplikace pro memorizaci slov. Funguje tak, že vybere náhodné slovo ze sady a zeptá se na odpověď.
+    Kromě slov a jejich odpovědí aplikace nabízí také „zadní strany“ (vytvořené pro anglický význam slov s kanji) a nápovědy. To vše je plně přizpůsobitelné.
+    Tenhle projekt jsem vytvořil pro vlastní použití, ale pokud byste chtěli, můžete si ho taky stáhnout z Githubu a volně používat.`
+    document.getElementById("langs-wordgrind-softewaretitle").innerHTML = `Použitý software`
+    document.getElementById("langs-wordgrind-link").innerHTML = `Get WordGrind on <u>GitHub</u>`
+}
+
+
+
+function langizewordgrind() {
+    if(currentlang == "EN") {
+        document.getElementById("langs-wordgrind-time").innerHTML = `October 2024`
+        document.getElementById("langs-wordgrind-category").innerHTML = `Category:<span class="project-element-subtext" style="margin-left: 10px;">Web Design</span>`
+        document.getElementById("langs-wordgrind-date").innerHTML = `Date added:<span class="project-element-subtext" style="margin-left: 10px;">9.10.2024</span>`
+        document.getElementById("langs-wordgrind-abouttitle").innerHTML = `About the project`
+        document.getElementById("langs-wordgrind-abouttext").innerHTML = `
+        WordGrind (言葉作業ゲー) is a simple web app for word memorization. It works by randomly picking a word from a deck and asking for an answer.
+        Besides words and their answers there are also "back sides" (made for english meaning of kanji words) and hints, which can all be fully customized.
+        It's a project that I made for my personal use, however if you want, you can also download it from GitHub and use it freely.`
+        document.getElementById("langs-wordgrind-softewaretitle").innerHTML = `Software used`
+        document.getElementById("langs-wordgrind-link").innerHTML = `Get WordGrind on <u>GitHub</u>`
+    } else if(currentlang == "SK") {
+        document.getElementById("langs-wordgrind-time").innerHTML = `Oktober 2024`
+        document.getElementById("langs-wordgrind-category").innerHTML = `Kategória:<span class="project-element-subtext" style="margin-left: 10px;">Web Design</span>`
+        document.getElementById("langs-wordgrind-date").innerHTML = `Dátum pridania:<span class="project-element-subtext" style="margin-left: 10px;">9.10.2024</span>`
+        document.getElementById("langs-wordgrind-abouttitle").innerHTML = `O projekte`
+        document.getElementById("langs-wordgrind-abouttext").innerHTML = `
+        WordGrind (言葉作業ゲー) je jednoduchá webová aplikácia pre memorizáciu slov. Funguje tak, že vyberie náhodné slovo zo sady a bude si pýtať odpoveď.
+        Okrem slov a ich odpovedí aplikácia ponúka aj "zadné strany" (urobené pre anglický význam slov s kanji) a nápovedy. Všetko z toho sa dá plne prispôsobiť.
+        Tento projekt som vytvoril pre svoje vlastné používanie, ale ak by ste chceli, tak asi ho môžete aj vy stiahnuť z Githubu a voľne ho používať.`
+        document.getElementById("langs-wordgrind-softewaretitle").innerHTML = `Použitý software`
+        document.getElementById("langs-wordgrind-link").innerHTML = `Získajte WordGrind na <u>GitHube</u>`
+    } else if(currentlang == "CZ") {
+        document.getElementById("langs-wordgrind-time").innerHTML = `Říjen 2024`
+        document.getElementById("langs-wordgrind-category").innerHTML = `Kategorie:<span class="project-element-subtext" style="margin-left: 10px;">Web Design</span>`
+        document.getElementById("langs-wordgrind-date").innerHTML = `Datum přidání:<span class="project-element-subtext" style="margin-left: 10px;">9.10.2024</span>`
+        document.getElementById("langs-wordgrind-abouttitle").innerHTML = `O projektu`
+        document.getElementById("langs-wordgrind-abouttext").innerHTML = `
+        WordGrind (言葉作業ゲー) je jednoduchá webová aplikace pro memorizaci slov. Funguje tak, že vybere náhodné slovo ze sady a zeptá se na odpověď.
+        Kromě slov a jejich odpovědí aplikace nabízí také „zadní strany“ (vytvořené pro anglický význam slov s kanji) a nápovědy. To vše je plně přizpůsobitelné.
+        Tenhle projekt jsem vytvořil pro vlastní použití, ale pokud byste chtěli, můžete si ho taky stáhnout z Githubu a volně používat.`
+        document.getElementById("langs-wordgrind-softewaretitle").innerHTML = `Použitý software`
+        document.getElementById("langs-wordgrind-link").innerHTML = `Get WordGrind on <u>GitHub</u>`
+    }
+}
