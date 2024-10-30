e58d83891b5b43f40395f30f6e59e1f0f5ff9613
Richard Sepši <appsepsi@gmail.com>
2024-10-30 21:26:49 +0100

    updated langdata for seaside cliffs

--- jslangadata/langdata_seasidecliffs.js
+++ jslangadata/langdata_seasidecliffs.js
@@ -15,28 +15,36 @@ if(currentlang == "EN") {
     `
     document.getElementById("langs-seasidecliffs-softewaretitle").innerHTML = `Software used`
 } else if(currentlang == "SK") {
-    document.getElementById("langs-seasidecliffs-time").innerHTML = `Od Februára do Júna 2022`
+    document.getElementById("langs-seasidecliffs-time").innerHTML = `Od Novembra 2023 do Októbra 2024`
     document.getElementById("langs-seasidecliffs-category").innerHTML = `Kategória:<span class="project-element-subtext" style="margin-left: 10px;">3D Projekty</span>`
-    document.getElementById("langs-seasidecliffs-date").innerHTML = `Dátum pridania:<span class="project-element-subtext" style="margin-left: 10px;">1.11.2023</span>`
+    document.getElementById("langs-seasidecliffs-date").innerHTML = `Dátum pridania:<span class="project-element-subtext" style="margin-left: 10px;">30.10.2024</span>`
     document.getElementById("langs-seasidecliffs-abouttitle").innerHTML = `O projekte`
     document.getElementById("langs-seasidecliffs-about").innerHTML = `
-    Toto je jeden z prvých 3D projektov, ktorý sa mi podarilo dokončiť. Ide o novú stanicu hypotetického metra v Bratislave v archviz štýle.
-    <div style="height: 12px;"></div>
-    Všetko bolo modelované v programe Cinema 4D a zvizualizované v Blenderi. Všetky textúry sú moje vlastné, okrem dlažby, ktorá pochádza zo stránky AmbientCG.
-    <div style="height: 12px;"></div>
-    V budúcnosti by som rád tento projekt prerobil a dal mu nový, lepší vzhľad.`
+    Na tomto projekte som pôvodne začal robiť aby neskôr vymenil môj strý banner.
+    Nie len, že som mu chcel dať nový vzhľad, ale chcel som aj aby v ňom boli použité iba moje assety a aby som sa počas práce niečo naučil
+    Myslím si, že tento účel projekt jednoznačne splnil. Nápad na vzhľad projektu som dostal z fotky pláže Broken Beach na Bali a chcel som vytvoriť niečo podobné.
+    Začal som z blockoutovaním scény a potom to už bolo len o tvorbe assetov.
+    Bohu žiaľ to trvalo veľmi dlho, keďže som chcel používať iba moje vlastné assety a nemal som čas na to, aby som na projekte stále pracoval.
+    Taktiež môj počítač nedokázal držať krok s komplexnosťou scény a takmer sa mi ani nepodarilo spraviť výsledný render, kvôli nedostatku vram.
+    Napriek všetkým tažkostiam som rád, že som projekt spravil, keďže som sa veľa naučil a budem lepšie vedieť ako pristupovať k tvorbe podobných projektov v budúcnosti.
+    Dúfam, že sa vám projekt bude páčiť.
+    `
     document.getElementById("langs-seasidecliffs-softewaretitle").innerHTML = `Použitý software`
 } else if(currentlang == "CZ") {
-    document.getElementById("langs-seasidecliffs-time").innerHTML = `Od Února do Června 2022`
+    document.getElementById("langs-seasidecliffs-time").innerHTML = `Od Listopadu 2023 do Října 2024`
     document.getElementById("langs-seasidecliffs-category").innerHTML = `Kategorie:<span class="project-element-subtext" style="margin-left: 10px;">3D Projekty</span>`
-    document.getElementById("langs-seasidecliffs-date").innerHTML = `Datum přidání:<span class="project-element-subtext" style="margin-left: 10px;">1.11.2023</span>`
+    document.getElementById("langs-seasidecliffs-date").innerHTML = `Datum přidání:<span class="project-element-subtext" style="margin-left: 10px;">30.10.2024</span>`
     document.getElementById("langs-seasidecliffs-abouttitle").innerHTML = `O projektu`
     document.getElementById("langs-seasidecliffs-about").innerHTML = `
-    Toto je jeden z prvních 3D projektů, který se mi podařilo dokončit. Jedná se o novou hypotetickou stanici metra v Bratislavě ve archviz stylu.
-    <div style="height: 12px;"></div>
-    Vše bylo vymodelováno v Cinemě 4D a vizualizováno v Blenderu. Všechny textury jsou mé vlastní, kromě dlažby, která pochází z webu AmbientCG.
-    <div style="height: 12px;"></div>
-    V budoucnu bych chtěl tento projekt přepracovat a dát mu nový, lepší vzhled.`
+    Původně jsem na tomto projektu začal pracovat, aby později nahradil můj starý banner.
+    Nejenže jsem mu chtěl dát nový vzhled, ale také jsem chtěl, aby používal pouze mé assety a abych se při práci na něm něco naučil.
+    Myslím, že tenhle účel projekt rozhodně splnil. Nápad na vzhled projektu jsem dostal z fotografie pláže Broken Beach na Bali a chtěl jsem vytvořit něco podobného.
+    Začal jsem od blockoutvání scény a pak už to bylo jen o vytváření assetů.
+    Bohužel to trvalo velmi dlouho, protože jsem chtěl použít pouze své vlastní assety a neměl jsem čas na projektu stále pracovat.
+    Také můj počítač nezvládal náročnost scény a málem jsem ani nemohl udělat finální render, kvůli nedostatku vram.
+    I přes všechny potíže jsem rád, že jsem se do projektu pustil, protože jsem se hodně naučil a budu lépe vědět, jak k tvorbě podobných projektů přistupovat v budoucnu.
+    Doufám, že se vám projekt bude líbit.
+    `
     document.getElementById("langs-seasidecliffs-softewaretitle").innerHTML = `Použitý software`
 }
 
@@ -44,41 +52,52 @@ if(currentlang == "EN") {
 
 function langizeseasidecliffs() {
     if(currentlang == "EN") {
-        document.getElementById("langs-seasidecliffs-time").innerHTML = `From February to June 2022`
+        document.getElementById("langs-seasidecliffs-time").innerHTML = `From November 2023 to October 2024`
         document.getElementById("langs-seasidecliffs-category").innerHTML = `Category:<span class="project-element-subtext" style="margin-left: 10px;">3D Project</span>`
-        document.getElementById("langs-seasidecliffs-date").innerHTML = `Date added:<span class="project-element-subtext" style="margin-left: 10px;">1.11.2023</span>`
+        document.getElementById("langs-seasidecliffs-date").innerHTML = `Date added:<span class="project-element-subtext" style="margin-left: 10px;">30.10.2024</span>`
         document.getElementById("langs-seasidecliffs-abouttitle").innerHTML = `About the project`
         document.getElementById("langs-seasidecliffs-about").innerHTML = `
-        This project is one of the first 3D projects that I managed to finish. It's supposed to be a newly built metro station in a hypothetical metro in Bratislava
-        in an archviz style.
-        <div style="height: 12px;"></div>
-        Everything was modeled in Cinema 4D and texured/rendered in Blender. All of the materials are my own except the floor, which comes from AmbientCG.
-        <div style="height: 12px;"></div>
-        In the future I would like to rework this project a bit to give it a new look.`
+        This is a project I originally started working on to be a replacement for my old banner.
+        Not only did I want to give it a new look, but I also wanted to use only my own assets and learn some things along the way.
+        I feel like it has definitely served that purpouse. I got the idea for the look from a photo of Broken Beach on Bali and I wanted to create something similar.
+        I started with blocking out the scene and then it was just about creating the assets.
+        It unfortunately took a really long time as I wanted each asset to be my own and I couldn't work on the project all the time.
+        I also struggled with my computer not beeing able to keep up with the scene's complexity and I was barely able to make the final render because of my GPUs very limited vram.
+        Despite all of the struggles, I'm glad I made the project as was able to learn a lot and I'll know how to better approach creating such a project in the future.
+        I hope you find it interesting.
+        `
         document.getElementById("langs-seasidecliffs-softewaretitle").innerHTML = `Software used`
     } else if(currentlang == "SK") {
-        document.getElementById("langs-seasidecliffs-time").innerHTML = `Od Februára do Júna 2022`
+        document.getElementById("langs-seasidecliffs-time").innerHTML = `Od Novembra 2023 do Októbra 2024`
         document.getElementById("langs-seasidecliffs-category").innerHTML = `Kategória:<span class="project-element-subtext" style="margin-left: 10px;">3D Projekty</span>`
-        document.getElementById("langs-seasidecliffs-date").innerHTML = `Dátum pridania:<span class="project-element-subtext" style="margin-left: 10px;">1.11.2023</span>`
+        document.getElementById("langs-seasidecliffs-date").innerHTML = `Dátum pridania:<span class="project-element-subtext" style="margin-left: 10px;">30.10.2024</span>`
         document.getElementById("langs-seasidecliffs-abouttitle").innerHTML = `O projekte`
         document.getElementById("langs-seasidecliffs-about").innerHTML = `
-        Toto je jeden z prvých 3D projektov, ktorý sa mi podarilo dokončiť. Ide o novú stanicu hypotetického metra v Bratislave v archviz štýle.
-        <div style="height: 12px;"></div>
-        Všetko bolo modelované v programe Cinema 4D a zvizualizované v Blenderi. Všetky textúry sú moje vlastné, okrem dlažby, ktorá pochádza zo stránky AmbientCG.
-        <div style="height: 12px;"></div>
-        V budúcnosti by som rád tento projekt prerobil a dal mu nový, lepší vzhľad.`
+        Na tomto projekte som pôvodne začal robiť aby neskôr vymenil môj strý banner.
+        Nie len, že som mu chcel dať nový vzhľad, ale chcel som aj aby v ňom boli použité iba moje assety a aby som sa počas práce niečo naučil
+        Myslím si, že tento účel projekt jednoznačne splnil. Nápad na vzhľad projektu som dostal z fotky pláže Broken Beach na Bali a chcel som vytvoriť niečo podobné.
+        Začal som z blockoutovaním scény a potom to už bolo len o tvorbe assetov.
+        Bohu žiaľ to trvalo veľmi dlho, keďže som chcel používať iba moje vlastné assety a nemal som čas na to, aby som na projekte stále pracoval.
+        Taktiež môj počítač nedokázal držať krok s komplexnosťou scény a takmer sa mi ani nepodarilo spraviť výsledný render, kvôli nedostatku vram.
+        Napriek všetkým tažkostiam som rád, že som projekt spravil, keďže som sa veľa naučil a budem lepšie vedieť ako pristupovať k tvorbe podobných projektov v budúcnosti.
+        Dúfam, že sa vám projekt bude páčiť.
+        `
         document.getElementById("langs-seasidecliffs-softewaretitle").innerHTML = `Použitý software`
     } else if(currentlang == "CZ") {
-        document.getElementById("langs-seasidecliffs-time").innerHTML = `Od Února do Června 2022`
+        document.getElementById("langs-seasidecliffs-time").innerHTML = `Od Listopadu 2023 do Října 2024`
         document.getElementById("langs-seasidecliffs-category").innerHTML = `Kategorie:<span class="project-element-subtext" style="margin-left: 10px;">3D Projekty</span>`
-        document.getElementById("langs-seasidecliffs-date").innerHTML = `Datum přidání:<span class="project-element-subtext" style="margin-left: 10px;">1.11.2023</span>`
+        document.getElementById("langs-seasidecliffs-date").innerHTML = `Datum přidání:<span class="project-element-subtext" style="margin-left: 10px;">30.10.2024</span>`
         document.getElementById("langs-seasidecliffs-abouttitle").innerHTML = `O projektu`
         document.getElementById("langs-seasidecliffs-about").innerHTML = `
-        Toto je jeden z prvních 3D projektů, který se mi podařilo dokončit. Jedná se o novou hypotetickou stanici metra v Bratislavě ve archviz stylu.
-        <div style="height: 12px;"></div>
-        Vše bylo vymodelováno v Cinemě 4D a vizualizováno v Blenderu. Všechny textury jsou mé vlastní, kromě dlažby, která pochází z webu AmbientCG.
-        <div style="height: 12px;"></div>
-        V budoucnu bych chtěl tento projekt přepracovat a dát mu nový, lepší vzhled.`
+        Původně jsem na tomto projektu začal pracovat, aby později nahradil můj starý banner.
+        Nejenže jsem mu chtěl dát nový vzhled, ale také jsem chtěl, aby používal pouze mé assety a abych se při práci na něm něco naučil.
+        Myslím, že tenhle účel projekt rozhodně splnil. Nápad na vzhled projektu jsem dostal z fotografie pláže Broken Beach na Bali a chtěl jsem vytvořit něco podobného.
+        Začal jsem od blockoutvání scény a pak už to bylo jen o vytváření assetů.
+        Bohužel to trvalo velmi dlouho, protože jsem chtěl použít pouze své vlastní assety a neměl jsem čas na projektu stále pracovat.
+        Také můj počítač nezvládal náročnost scény a málem jsem ani nemohl udělat finální render, kvůli nedostatku vram.
+        I přes všechny potíže jsem rád, že jsem se do projektu pustil, protože jsem se hodně naučil a budu lépe vědět, jak k tvorbě podobných projektů přistupovat v budoucnu.
+        Doufám, že se vám projekt bude líbit.
+        `
         document.getElementById("langs-seasidecliffs-softewaretitle").innerHTML = `Použitý software`
     }
 }
